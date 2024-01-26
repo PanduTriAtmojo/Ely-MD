ebff6965b1dcb321c1edd526dc3be6f8bc73ae03
pand <pandutriatmojo6@gmail.com>
2024-01-26 23:29:00 +0700

    Update plugins ytdownload

--- plugins/download/ytplay.js
+++ plugins/download/ytplay.js
@@ -1,13 +1,11 @@
-import { youtubeSearch, youtubedl } from '@bochilteam/scraper-sosmed'
-import { somematch, isUrl, niceBytes } from '../../lib/func.js'
+import { youtubeSearch } from '@bochilteam/scraper-sosmed'
+import { isUrl } from '../../lib/func.js'
 
 let handler = async (m, { conn, text, args, usedPrefix, command }) => {
 	if (!text) throw `Example: ${usedPrefix + command} Sia Unstopable`
-	let url = ''
 	if (isUrl(text)) {
-		url = text
 		try {
-			let anu = await youtubeSearch(url)
+			let anu = await youtubeSearch(text)
 			anu = anu.video[0]
 			let txt = `📌 *${anu.title}*\n\n`
 				+ `🪶 *Author :* ${anu.authorName}\n`
@@ -18,40 +16,42 @@ let handler = async (m, { conn, text, args, usedPrefix, command }) => {
 			await conn.sendMsg(m.chat, { image: { url: anu.thumbnail.split("?")[0] }, caption: txt }, { quoted: m })
 		} catch (e) {
 			console.log(e)
-			return m.reply('invalid url')
+			try {
+				let anu = await (await fetch(`https://api.akuari.my.id/downloader/yt1?link=${args[0]}`)).json()
+				let txt = `📌 *${anu.info.title}*\n\n`
+					+ `🪶 *Author :* ${anu.info.channel}\n`
+					+ `⏲️ *Published :* ${anu.info.uploadDate}\n`
+					+ `👁️ *Views :* ${anu.info.views}\n`
+					+ `🌀 *Url :* ${text}`
+				await conn.sendMsg(m.chat, { image: { url: anu.info.thumbnail }, caption: txt }, { quoted: m })
+			} catch (e) {
+				console.log(e)
+				throw 'invalid url / internal server error.'
+			}
 		}
 	} else {
 		try {
 			let anu = await youtubeSearch(text)
-			anu = anu.video[0]
-			url = anu.url
-			let txt = `📌 *${anu.title}*\n\n`
-				+ `🪶 *Author :* ${anu.authorName}\n`
-				+ `⏲️ *Published :* ${anu.publishedTime}\n`
-				+ `⌚ *Duration :* ${anu.durationH}\n`
-				+ `👁️ *Views :* ${anu.viewH}\n`
-				+ `🌀 *Url :* ${url}`
-			await conn.sendMsg(m.chat, { image: { url: anu.thumbnail.split("?")[0] }, caption: txt }, { quoted: m })
+			let txt = `*Hasil : ${text}*`
+			for (let i of anu.video) {
+				txt += `\n\n🎯 *${i.title}*\n`
+					+ `🪶 Author : ${i.authorName}\n`
+					+ `⏰ Duration : ${i.durationH}\n`
+					+ `${i.publishedTime ? `${i.publishedTime.split(" ")[0] != 'Streamed' ? `🚀 Uploaded ${i.publishedTime}\n` : `🚀 ${i.publishedTime}\n`}` : ''}`
+					+ `😎 View : ${i.viewH}\n`
+					+ `🌀 Url : ${i.url}\n`
+					+ `───────────────────`
+			}
+			await conn.sendMsg(m.chat, { image: { url: anu.video[0].thumbnail.split("?")[0] }, caption: txt }, { quoted: m })
 		} catch (e) {
 			console.log(e)
-			return m.reply(`Tidak ditemukan hasil.`)
+			throw 'not found / internal server error.'
 		}
-	}
-	if (!url) return
-	try {
-		let res = await youtubedl(url)
-		let data = res.audio[Object.keys(res.audio)[0]]
-		let site = await data.download()
-		if (data.fileSize > 400000) return m.reply(`Filesize: ${data.fileSizeH}\nTidak dapat mengirim, maksimal file 400 MB`)
-		await conn.sendMsg(m.chat, { audio: { url: site }, mimetype: 'audio/mpeg' }, { quoted: m })
-	} catch (e) {
-		console.log(e)
-		m.reply(e)
 	}
 }
 
-handler.menudownload = ['ytplay <teks> / <url>']
+handler.menudownload = ['ytsearch <teks> / <url>']
 handler.tagsdownload = ['search']
-handler.command = /^(play|(play)?yt(play|dl)?)$/i
+handler.command = /^((search)?yt(s(earch)?)|youtube)$/i
 
 export default handler
